061cc9dc88d85ad48aaa841dd09483b67e1a1728
Leo Izen <leo.izen@gmail.com>
2016-11-08 19:35:19 -0500

    fix more blockquotes

--- tumblr/fix_blockquote.js
+++ tumblr/fix_blockquote.js
@@ -1,20 +1,24 @@
 function fixBlockQuote(bq){
-	if (bq.parentElement.tagName.toLowerCase() === "blockquote"){
 		var ptag = bq.previousElementSibling;
 		if (ptag && ptag.firstElementChild){
 			var atag = ptag.firstElementChild;
 			if (atag.className.toLowerCase() === "tumblr_blog") {
-				fixBlockQuote(bq.parentElement);
-				var el = bq.parentElement.parentElement;
-				var par = bq.parentElement;
-				var first = bq.previousElementSibling;
-				var second = bq;
-				el.insertBefore(second, par.previousElementSibling);
-				if (first){
-					el.insertBefore(first, second);
-				}
-				second.style.borderBottom = "1px solid #000000";
-				return true;
+				bq.style.borderBottom = "1px solid #000000";
+					if (bq.parentElement.tagName.toLowerCase() === "blockquote"){
+						fixBlockQuote(bq.parentElement);
+						var el = bq.parentElement.parentElement;
+						var par = bq.parentElement;
+						var first = bq.previousElementSibling;
+						var second = bq;
+						el.insertBefore(second, par.previousElementSibling);
+						if (first){
+							el.insertBefore(first, second);
+						}
+						return true;
+					} else {
+						return false;
+					}
+				
 			} else {
 				return false;
 			}
