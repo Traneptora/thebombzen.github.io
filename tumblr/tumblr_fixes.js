cc9a666efc51e99ca0b3c126ab8b363a5ba51ee3
Leo Izen <leo.izen@gmail.com>
2016-11-08 21:23:43 -0500

    Fixed photoset height errors

--- tumblr/tumblr_fixes.js
+++ tumblr/tumblr_fixes.js
@@ -7,6 +7,11 @@ function tumblrFixes(){
 			div.parentElement.removeChild(div);
 		}
 	}
+	if (div.className.toLowerCase().equals("html_photoset")){
+		var iframe = div.getElementsByTagName("iframe")[0];
+		iframe.style.height = "100%";
+		div.style.height = iframe.contentDocument.getElementsByTagName("html")[0].scrollHeight + "px";
+	}
 }
 
 
