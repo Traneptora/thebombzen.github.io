f5479c4a3c335959d4aecf0536ba4aa5a615c769
Leo Izen <leo.izen@gmail.com>
2016-11-08 20:47:11 -0500

    fixed wrong array index

--- tumblr/fix_blockquote.js
+++ tumblr/fix_blockquote.js
@@ -5,8 +5,8 @@ function fixBlockQuote(bq){
 		
 		if (atag) {
 			var uname = atag.innerHTML; 
-			var mat = atag.href.match(/^https?:\/\/(www.)?(.*).tumblr.com\/post\//);
-			if (uname === mat[2]){
+			var mat = atag.href.match(/^https?:\/\/(.*)\.tumblr\.com\/post\//);
+			if (uname === mat[1]){
 				if (!atag.classList.contains("tumblr_blog")){
 					atag.classList.add("tumblr_blog");
 				}
