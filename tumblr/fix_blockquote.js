5ff87d15a5a8732b8e7d0bec91821b17504f735f
Leo Izen <leo.izen@gmail.com>
2016-11-08 19:15:34 -0500

    Fixed fix_blockquite... I hope

--- tumblr/fix_blockquote.js
+++ tumblr/fix_blockquote.js
@@ -1,15 +1,25 @@
 function fixBlockQuote(bq){
 	if (bq.parentElement.tagName.toLowerCase() === "blockquote"){
-		fixBlockQuote(bq.parentElement);
-		var el = bq.parentElement.parentElement;
-		var par = bq.parentElement;
-		var first = bq.previousElementSibling;
-		var second = bq;
-		el.insertBefore(second, par.previousElementSibling);
-		if (first){
-			el.insertBefore(first, second);
+		var atag = bq.parentElement.firstElementChild.firstElementChild;
+		if (atag){
+			if (atag.className.toLowerCase() === "tumblr_blog") {
+				fixBlockQuote(bq.parentElement);
+				var el = bq.parentElement.parentElement;
+				var par = bq.parentElement;
+				var first = bq.previousElementSibling;
+				var second = bq;
+				el.insertBefore(second, par.previousElementSibling);
+				if (first){
+					el.insertBefore(first, second);
+				}
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
 		}
-		return true;
+		
 	} else {
 		return false;
 	}
